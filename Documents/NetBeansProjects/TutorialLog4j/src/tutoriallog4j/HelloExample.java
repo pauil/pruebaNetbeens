c6b250b77f2386fe74b6684af199e84b065ec4c4
Demo-master <dcasaoliva@gmail.com>
2016-04-27 13:37:51 +0200

    ESto es una prueba para subir proyectos de netbeens a github

---
+++ Documents/NetBeansProjects/TutorialLog4j/src/tutoriallog4j/HelloExample.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutoriallog4j;
+
+/**
+ *
+ * @author usu21
+ */
+import org.apache.log4j.Logger;
+
+public class HelloExample {
+    
+
+public class HelloExample{
+	
+	final static Logger logger = Logger.getLogger(HelloExample.class);
+	
+	public static void main(String[] args) {
+	
+		HelloExample obj = new HelloExample();
+		obj.runMe("mkyong");
+		
+	}
+	
+	private void runMe(String parameter){
+		
+		if(logger.isDebugEnabled()){
+			logger.debug("This is debug : " + parameter);
+		}
+		
+		if(logger.isInfoEnabled()){
+			logger.info("This is info : " + parameter);
+		}
+		
+		logger.warn("This is warn : " + parameter);
+		logger.error("This is error : " + parameter);
+		logger.fatal("This is fatal : " + parameter);
+		
+	}
+	
+}
+    
+}
